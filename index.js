83c0967f294903ac6dd269ea185a62c625074bf4
manuel <manu@correo.com>
2020-04-29 19:55:56 -0600

    renombrar app a server para una mejor nomenclatura

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-//obtenemos app y PORT mediante destructuring
-//app y PORT vienen del archivo index de la carpeta server
+//obtenemos server y PORT mediante destructuring
+//server y PORT vienen del archivo index de la carpeta server
 //No es necesario agregar index cuando se solicita con require
-const {app,PORT}=require('./server');
+const {server,PORT}=require('./server');
 
 //Encendemos el servidor
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+server.listen(PORT, () => console.log(`Example server listening at http://localhost:${PORT}`))
+
+
