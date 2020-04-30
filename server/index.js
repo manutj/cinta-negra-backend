53e8f6eb6fd23e261b586c0543ab8c523605f8e5
manuel <manu@correo.com>
2020-04-29 20:56:55 -0600

    agregando metodos de crear y buscar usuarios

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const server=express();
 const PORT=process.env.PORT || 3002;
 
+server.use(express.urlencoded({extended:true}));
+server.use(express.json());
+
 //Endpoints
 server.get('/', (req, res) => res.send('Hello World!'))
 
@@ -12,6 +15,23 @@ console.log(req.query);
 res.send('Cool')
 });
 
+const Users=require('../models/Users');
+
+//CRUD USUARIOS
+
+//CREATE
+server.post('/api/v1/users',(req,res)=>{
+Users.create(req.body)
+.then(user=>res.status(201).send(user))
+.catch(err=>res.status(400).send({message:'Error creando el usuario',err}));
+});
+
+//GET ALL
+server.get('/api/v1/users',(req,res)=>{
+    Users.find()
+    .then(user=>res.status(200).send(user))
+    .catch(err=>res.status(404).send({message:'Usuario no encontrado',err}));
+    });
 //Exportar server para requerirlo desde otros archivos
 module.exports={server, PORT};
 
