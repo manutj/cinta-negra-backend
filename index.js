4292297e90c40393d2b662ca2ba08f821b3c1843
manuel <manu@correo.com>
2020-04-29 18:24:01 -0600

    Refactorizar configuracion de servidor

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const express=require('express');
-const app=express();
-const PORT=process.env.PORT || 3002;
-
-//Endpoints
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.post('/api/v1/movies', (req, res) => res.status(201).send({movies : []}));
-
-app.get('/api/v1/temario/:id', (req, res) => {
-console.log(req.query);
-res.send('Cool')
-});
-
-
-
+//obtenemos app y PORT mediante destructuring
+//app y PORT vienen del archivo index de la carpeta server
+//No es necesario agregar index cuando se solicita con require
+const {app,PORT}=require('./server');
 
 //Encendemos el servidor
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
