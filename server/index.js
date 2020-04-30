e92f05949cc20cd7ed6325ef038e5ae47897b7b2
manuel <manu@correo.com>
2020-04-30 14:10:13 -0600

    agregando metodos crud restantes

--- server/index.js
+++ server/index.js
@@ -32,6 +32,29 @@ server.get('/api/v1/users',(req,res)=>{
     .then(user=>res.status(200).send(user))
     .catch(err=>res.status(404).send({message:'Usuario no encontrado',err}));
     });
+
+//GET ONE
+server.get('/api/v1/users/:id',(req,res)=>{
+    Users.findById(req.params.id)
+    .then(user=>{res.status(200).send(user)})
+    .catch(err=>res.status(404).send({message:'Usuario no encontrado'}))
+});
+
+//UPDATE
+server.patch('/api/v1/users/:id',(req,res)=>{
+    Users.findByIdAndUpdate(req.params.id,req.body,{new:true})
+    .then(user=>{res.status(200).send(user)})
+    .catch(err=>res.status(404).send({message:'No se pudo actualizar el usuario'}))
+});
+
+//DELETE
+server.delete('/api/v1/users/:id',(req,res)=>{
+    Users.findByIdAndDelete(req.params.id)
+    .then(user=>{res.status(200).send('Usuario eliminado correctamente')})
+    .catch(err=>res.status(404).send({message:'Usuario no encontrado'}))
+});
+
+
 //Exportar server para requerirlo desde otros archivos
 module.exports={server, PORT};
 
