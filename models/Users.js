6499f6c55d5974d77d6f7e80f8f5e61cbb8f68f1
manuel <manu@correo.com>
2020-05-05 16:56:50 -0600

    Agregando campo de password y hasheo para encriptarlos

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,7 @@
 const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const usersSchema=new mongoose.Schema({
     first_name:{
@@ -12,12 +15,37 @@ const usersSchema=new mongoose.Schema({
     email:{
         type:String,
         required:true,
+        unique:true
     },
     is_active:{
         type:Boolean,
         default:true,
     },
+    password:{
+        type:String,
+    }
 });
+
+usersSchema.pre('save', function(next) {
+    const user = this;
+  
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+  
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+  
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+  
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+    });
+  });
 
 const Users=mongoose.model('User',usersSchema);
 module.exports=Users;
