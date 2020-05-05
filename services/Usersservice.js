0799978990317a03c23266ba33245212988ba779
manuel <manu@correo.com>
2020-05-05 17:00:27 -0600

    agregar servicios para comparar correos y contraseñas

--- services/Usersservice.js
+++ services/Usersservice.js
@@ -1,4 +1,5 @@
 const Users=require('../models/Users');
+const bcrypt = require('bcrypt');
 module.exports={
     create:(body)=>Users.create(body),
 
@@ -9,6 +10,13 @@ module.exports={
     update: (id,body)=>Users.findByIdAndUpdate(id,body,{new:true}),
         
 
-    delete: (id)=>Users.findByIdAndDelete(id)
+    delete: (id)=>Users.findByIdAndDelete(id),
+
+    findByEmail:(email)=>Users.findOne({email}),
+
+    comparePasswords:(candidatePassword, password) => {
+        console.log('🌎', candidatePassword, password);
+        return bcrypt.compareSync(candidatePassword, password);
+      }
         
     }
