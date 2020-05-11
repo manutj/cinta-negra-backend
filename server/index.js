25f374b30b1fdc43eae2f855d0ac6ae56abc4374
manuel <manu@correo.com>
2020-05-11 15:37:54 -0600

    Manejando errores de validación de campos

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const server=express();
 const PORT=process.env.PORT || 3002;
+const {errors}=require('celebrate');
 
 server.use(express.urlencoded({extended:true}));
 server.use(express.json());
@@ -10,7 +11,6 @@ server.get('/', (req, res) => res.send('Hello World!'))
 
 server.use('/api/v1',require('../router'))
 
+server.use(errors());
 //Exportar server para requerirlo desde otros archivos
 module.exports={server, PORT};
-
-
