7090f244765b1c362b060d9c59240f3159fee2c1
manuel <manu@correo.com>
2020-05-17 00:54:37 -0600

    validador de campos de rol de usuario

---
+++ validators/RolesValidator.js
@@ -0,0 +1,15 @@
+const {celebrate, Joi, Segments} =require('celebrate');
+
+module.exports={
+    create:celebrate({
+        [Segments.BODY]: Joi.object().keys({
+         title:Joi.string().required(),
+          })
+        }),
+    
+    update:celebrate({
+        [Segments.BODY]: Joi.object().keys({
+        title:Joi.string(),
+          })
+        }),
+}
